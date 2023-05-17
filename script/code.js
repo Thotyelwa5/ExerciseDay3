4e13c8729d4faa113032950c640256fdd7dcdf82
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-05-17 09:43:36 +0200

    update

---
+++ script/code.js
@@ -0,0 +1,19 @@
+let myVariable = "I love coding"
+console.log(typeof myVariable);
+myVariable = 567 //giving a variable numbers
+console.log(typeof myVariable);
+//creating a variable with ever data type
+let dataTypeVariable = "string";
+console.log(typeof dataTypeVariable);
+let numberVariable = 567;
+console.log(typeof numberVariable);
+let booleanVariable = true;
+console.log(booleanVariable);
+let nullVariable = null;
+console.log(nullVariable);
+let undefinedVariable = undefined;
+console.log(undefinedVariable);
+let objectVariable = { key: "value" };
+console.log(objectVariable);
+let arrayVariable = [1, 2, 3];
+console.log(arrayVariable);
